e5d17519e1c0ba4dfdca8c3d4ae2ac86aaa534fb
davebb16 <david.billings@sceince.edu>
2024-08-20 13:22:13 -0500

    test push

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -2,6 +2,7 @@ import { createRootRoute, Link, Outlet } from '@tanstack/react-router'
 import { TanStackRouterDevtools } from '@tanstack/router-devtools'
 
 export const Route = createRootRoute({
+  //test
   component: () => (
     <div className="p-2 gap-2 flex flex-col flex-1 bg-orange-500">
       <div className="p-2 gap-2 flex flex-1 bg-red-500">
