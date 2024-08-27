3846647feb09805ad59f3557500356ccb745f774
David Billings <david.billings@goldkey.com>
2024-08-26 21:29:36 -0500

    changes

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,6 +1,11 @@
 import { Link } from '@tanstack/react-router'
 import Wrapper from './Wrapper'
 import logo from '../assets/img/logo2.png'
+import headerlogo from '../assets/img/header-logo.png'
+import headerlogov2 from '../assets/img/header-logo-v2.png'
+import og from '../assets/img/logo.png'
+
+
 import Typography from './Typography'
 
 
@@ -10,8 +15,15 @@ export default function Footer() {
       <Wrapper>
         <div className='border-t border-background-border py-6'>
           <img src={logo} className='w-24' alt='זהב' />
+          
+
           <Typography variant='heading4' fontWeight='normal' className='font-secular text-foreground'>לומד עברית</Typography>
       </div>
+      <img src={headerlogo} className='w-24' alt='זהב' />
+
+          <img src={og} className='w-48' alt='זהב' />
+          <img src={headerlogov2} className='w-24' alt='זהב' />
+
       </Wrapper>
     </header>
   )
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,7 @@
 import { Link } from '@tanstack/react-router'
 import Wrapper from './Wrapper'
 // import logo from '../assets/img/logo2.png'
-import magen from '../assets/img/magen-sm-flat.png'
+import logo from '../assets/img/header-logo-v2.png'
 
 import Typography from './Typography'
 import Button from './Button'
@@ -13,7 +13,7 @@ export default function Header() {
     <header className="border-b sticky top-0 z-10 backdrop-blur bg-gradient-to-tr from-violet-700/90 to-indigo-600/90 border-violet-500">
             
 
-      <Wrapper as='nav' className='flex justify-between py-3'>
+      <Wrapper as='nav' className='flex justify-between py-1'>
         <div className='flex items-center gap-8'>
       <Button onClick={() =>
             currentTheme.value === 'dark'
@@ -27,8 +27,8 @@ export default function Header() {
       </div>
       {/* <Link to="/"><img src={logo} className='w-20' alt='logo' /></Link> */}
             
-      <Link className='flex gap-2 items-center' to="/"><Typography variant='heading2' color='primaryForeground' fontWeight='normal' className='font-secular'> זהב</Typography>
-      <img src={magen} className='size-8' alt='logo' /></Link>
+      <Link className='flex gap-2 items-center' to="/">
+      <img src={logo} className='size-16' alt='זהב' /></Link>
       </Wrapper>
     </header>
   )
